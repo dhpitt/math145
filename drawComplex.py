a878a6c513230bb87d2b6712bfb192060b381340
dhpitt <david.h.pitt@gmail.com>
2021-02-05 15:18:50 -0800

    update

--- drawComplex.py
+++ drawComplex.py
@@ -4,24 +4,37 @@
 # to help me do my homeworks.
 
 import matplotlib.pylab as plt
+from matplotlib.pyplot import draw
 import numpy as np
 import math
 
+# ax = plt.subplot() # Turn on if you want 1 plot
 
-def complexCurve1(poly,res=314):
-    # Draws a curve for a given complex polynomial
+def drawGammaR_1(poly,r=1,res=314):
+    # Draws a curve gamma_r for a given complex polynomial
     # on the C plane.
+    # The function maps points on the unit circle
+    # to the complex plane using p: C --> C
+
+    # generate slices of the interval [0,1]
     tspace = np.linspace(0,1,res+1)
+
     Zs = []
     pZ = []
+    
+    # map t to the function r = 1
+
     for t in tspace:
-        Zs.append(np.exp(2*math.pi*1j*t))
+        Zs.append(r*np.exp(2*math.pi*1j*t))
 
     for z in Zs:
         pZ.append(poly(z))
 
     X = [x.real for x in pZ]
     Y = [x.imag for x in pZ]
+
+    # plot the figure
+    
     ax = plt.subplot()
     ax.plot(X,Y)
     ax.set_xlabel('$Re(z)$')
@@ -33,7 +46,7 @@ def complexCurve1(poly,res=314):
     ax.grid(which='major', alpha=0.5)
     ax.set_title(poly.latex_label)
     plt.show()
-    
+    #return X,Y
 
 class polynomial:
     # An object representing a complex polynomial.
@@ -48,10 +61,11 @@ class polynomial:
         return self.func(*args, **kwargs)
 
 p1 = polynomial(lambda z: z**2 - z, r'$p(z) = z^2  - z$')
-complexCurve1(p1)
+# complexCurve1(p1)
 
-p2 = polynomial(lambda z: z**4 - 2*z**3 + z**2 - z + 2, r'$p(z) = z^4 - 2z^3 + z^2 - z + 2$')
-complexCurve1(p2)
+p2 = polynomial(lambda z: z**4 + z**2 - z + 2, r'$p(z) = z^4 + z^2 - z + 2$')
+drawGammaR_1(p2)
+drawGammaR_1(p2,2)
 
 
 
